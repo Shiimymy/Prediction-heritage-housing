8794c2f697c71a9d88962d5f5a5f1dbd1fee8cb9
Eugénie Bvn <eugenie.b24@hotmail.fr>
2024-04-06 11:51:39 +0000

    Add Streamlit app_pages set up

---
+++ app_pages/page_summary.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+
+def page_summary_body():
+    """
+    Displays contents of the project summary page
+    """
+    st.write("### Quick Project Summary")
+
+    st.info(
+        f"**Project Terms & Jargons**\n\n"
+    )
+
+    # text based on README file - "Dataset Content" section
+    st.info(
+        f"**Project Dataset**\n"
+    )
+
+    # Link to README file, so the users can have access to
+    # full project documentation
+    st.write(
+        f"* For additional information, please visit and read the "
+        f"[Project README file]\
+        (https://github.com/Shiimymy/Prediction-heritage-housing).")
+
+    # copied from README file - "Business Requirements" section
+    st.success(
+        f"**Project Business Requirements**\n\n"
+
+        f"The project has 2 business requirements:\n"
+        f"* **- 1 -** The client is interested in discovering how the house\
+         attributes correlate with the sale price."
+        f" Therefore, the client expects data visualisations of the correlated\
+         variables against the sale price to show that.\n\n"
+        f"* **- 2 -** The client is interested in predicting the house sale\
+         price from her four inherited houses and any other house\
+         in Ames, Iowa."
+    )
