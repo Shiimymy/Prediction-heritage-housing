5874a0926c89a2a95f507c56afc27510480ed300
Eugénie Bvn <eugenie.b24@hotmail.fr>
2024-04-10 11:49:31 +0000

    Add project hypothesys dashboard page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_sales_price_study import page_sales_price_study_body
 from app_pages.page_predict_price_tool import page_predict_price_tool_body
-#from app_pages.page_project_hypothesis import page_project_hypothesis
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
 #from app_pages.page_ML_predict_price import page_ml_predict_price
 
 
@@ -15,7 +15,7 @@ app = MultiPage(app_name= "Heritage Housing Prediction") # Create an instance of
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Housing Sales Price Study", page_sales_price_study_body)
 app.add_page("Prediction Sales Tool", page_predict_price_tool_body)
-#app.add_page("Project Hypothesis and Validation", page_project_hypothesis)
+app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
 #app.add_page("ML: House Price Predictor", page_ml_predict_price)
 
 
---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+def page_project_hypothesis_body():
+    st.write("### Project Hypothesis and Validation")
+
+    # conclusions taken from "SalesPriceStudy" notebook
+    st.success(
+        f"We suspect that the size, the quality of the house and the year it was built impact the sale price \
+        and that the price correlates strongly with other features: \n"
+        f"* **It is correct**: The correlation study the Sales Price Study supports that."
+    )
+    
+    st.info(
+        f"**Hypothesis 1:** \n"
+        f"* We expect that the size of a house is positively correlated to the sale price.\
+        A larger house will tend to have a higher price.\
+        The features that confirm it are 'GrLivArea', '1stFlrSF' and 'TotalBsmtSF'. \n"
+
+        f"* Then we can also expect that the better the quality of the house, the higher the price too.\
+        It is confirmed thanks to the following features: 'OverallQual' and 'KitchenQual'. \n"
+
+        f"* Finally, we expect that the age of the house impact positively the sale price. \
+        It is confimed thanks to the 'YearBuilt' feature."
+    )
+
+    st.info(
+        f"**Hypothesis 2:** \n"
+        f"* When analysing the dataset, we suspect that the price of a house would correlate strongly with other different features: "
+        f"We can confirm that the price of a house correlates strongly with other different features: ['GarageYrBlt'], ['GarageArea'] and ['YearRemodAdd']. \
+        We can see that the Garage and remodel date are important features in the area of Ames, Iowa."
+    )
+
