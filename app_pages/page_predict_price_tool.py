a662f0b4ee3629c1b059c8fb40cada44d49959d2
Eugénie Bvn <eugenie.b24@hotmail.fr>
2024-04-10 11:12:34 +0000

    Add house prediction widgets

--- app_pages/page_predict_price_tool.py
+++ app_pages/page_predict_price_tool.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
-from src.data_management import load_pkl_file, load_heritage_data
-from src.machine_learning.predictive_analysis_ui import predict_inherited_house_price 
+from src.data_management import load_pkl_file, load_heritage_data, load_house_price_data
+from src.machine_learning.predictive_analysis_ui import predict_inherited_house_price, predict_price
+from datetime import date
 
 def page_predict_price_tool_body():
 
@@ -51,4 +52,98 @@ def page_predict_price_tool_body():
     )
     st.write("---")
 
+    # Generate Live Data
+    st.write("### House Price Predictor Tool")
+
+    st.write("#### Predict the sale price of a house\
+             based on these features:")
+    st.write("Fill in the form and click on the 'Predict Sale Price'\
+         button to get a prediction.")
+
+    X_live = DrawInputsWidgets()
+
+    # predict on live data
+    if st.button("Predict Sale Price"):
+        price_prediction = predict_price(
+            X_live, house_features, regression_pipe)
+
+        if price_prediction == 1:
+            predict_price(X_live, house_features, regression_pipe)
+
+
+
+def check_variables_for_UI(house_features):
+	st.write(f"* There are {len(house_features)} features for the UI: \n\n {house_features}")
     
+def DrawInputsWidgets():
+
+	# load dataset
+	df = load_house_price_data()
+	percentageMin, percentageMax = 0.4, 2.0
+
+    # we create input widgets only for 6 features	
+	col1, col2, col3, col4 = st.beta_columns(4)
+	col5, col6, col7, col8 = st.beta_columns(4)		
+
+	# create an empty DataFrame, which will be the live data
+	X_live = pd.DataFrame([], index=[0]) 
+	
+	# from here on we draw the widget based on the variable type (numerical or categorical)
+	# and set initial values
+	with col1:
+		feature = "YearBuilt"
+		st_widget = st.number_input(
+			label= "Original construction date",
+			min_value=int(df[feature].min()*percentageMin),
+            max_value=date.today().year,
+            value=int(df[feature].median()),
+            step=1
+			)
+		
+	X_live[feature] = st_widget
+
+	with col2:
+		feature = "TotalBsmtSF"
+		st_widget = st.number_input(
+			label= "Total square feet of basement area",
+			min_value= int(df[feature].min()*percentageMin), 
+			max_value= int(df[feature].max()*percentageMax),
+			value= int(df[feature].median()), 
+            step= 1
+			)
+	X_live[feature] = st_widget
+
+	with col3:
+		feature = "GarageArea"
+		st_widget = st.number_input(
+			label= "Size of garage in square feet",
+			min_value= int(df[feature].min()*percentageMin), 
+			max_value= int(df[feature].max()*percentageMax),
+			value= int(df[feature].median()), 
+            step= 1
+			)
+	X_live[feature] = st_widget
+
+	with col4:
+		feature = "2ndFlrSF"
+		st_widget = st.number_input(
+			label= "Second-floor square feet",
+			min_value= int(df[feature].min()*percentageMin), 
+			max_value= int(df[feature].max()*percentageMax),
+			value= 0, 
+            step= 1
+			)
+	X_live[feature] = st_widget
+
+	with col5:
+		feature = "LotArea"
+		st_widget = st.number_input(
+			label= "Lot size in square feet",
+			min_value= int(df[feature].min()*percentageMin), 
+			max_value= int(df[feature].max()*percentageMax),
+			value= int(df[feature].median()), 
+            step= 1
+			)
+	X_live[feature] = st_widget
+
+	return X_live
