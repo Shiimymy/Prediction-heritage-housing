ccf3df427f242d0e786db9fb0d6fa1c129b49adb
Eugénie Bvn <eugenie.b24@hotmail.fr>
2024-04-10 14:53:42 +0000

    Add pipeline performance info in ml technical page

--- app_pages/page_ml_predict_price.py
+++ app_pages/page_ml_predict_price.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from src.data_management import load_pkl_file
+from src.machine_learning.evaluate_reg import regression_performance, regression_evaluation_plots
+
 
 def page_ml_predict_price_body():
     """
@@ -27,7 +29,7 @@ def page_ml_predict_price_body():
     # display pipeline training summary conclusions
     st.success(
         f"The pipeline was shaped to have at least 0.75 accuracy in predicting the sales price of a property. "
-        f"This was done to answer the **Business Requirement 2**. \n\n"
+        f"This was done to answer the **Business Requirement 2**."
         f"* The pipeline performance on train and test set is 0.87 and 0.75, respectively."
     )
 
@@ -42,3 +44,18 @@ def page_ml_predict_price_body():
     st.write(X_train.columns.to_list())
     st.image(feature_importance)
     st.write("---")
+
+    st.write("### Pipeline Performance")
+
+    st.write("##### Performance goal of the predictions:\n")
+    st.write("* We agreed with the client an R2 score of at least 0.75 on the train set as well as on the test set.")
+    regression_performance(X_train=X_train, y_train=y_train,
+                        X_test=X_test, y_test=y_test,
+                        pipeline=pipeline)
+    st.write("We can see that the test set is slightly under the R2 score of 0.75 and the train set is well above.")
+    st.write("If we round the scores, we can say that the regressor pipeline reached the expected performance threshold. \n\n")
+
+    st.write("### Regression Performance Plots")
+    st.write("* The regression performance plots below indicate that our model,\
+         in most part, is able to predict sale prices well. The model looks less effective for houses with high prices though.")
+    regression_evaluation_plots(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, pipeline=pipeline)  
